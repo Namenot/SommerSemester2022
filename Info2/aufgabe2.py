4976686b658b586828cff9047b88c258ca50fd09
Namenot <aaron.sastry2@gmail.com>
2022-04-28 17:29:46 +0200

    finished homework 2

--- Info2/aufgabe2.py
+++ Info2/aufgabe2.py
@@ -13,6 +13,23 @@ def merge(s1, s2):
     
     returns s1 and s2 merged and sorted in ascending order """
 
+    c = list()
+    while(True):
+        if len(s1) == 0:
+            c = c + s2
+            break
+        if len(s2) == 0:
+            c = c + s1
+            break
+        
+        if s1[0] <= s2[0]:
+            c.append(s1.pop(0))
+        else:
+            c.append(s2.pop(0))
+
+    return c
+
+
 def quick_sort(s, l, r, pivot_function):
     """ s is of type 'list' with elements of type
     'str', 'float' or 'int'; l and r are of type 'int'
@@ -22,6 +39,32 @@ def quick_sort(s, l, r, pivot_function):
     
     returns s sorted in ascending order """
 
+
+    if(r-l) > 0:
+        j = pivot_function(l, r)
+        p = s[j]
+        i = l
+        j = r
+        while(True):
+            
+            while s[i] < p:
+                i+= 1
+            
+            while s[j] > p:
+                j-= 1
+            
+            if i <=j:
+                s[i], s[j] = s[j], s[i]
+                i+=1
+                j-=1
+
+            if i > j:
+                break
+        quick_sort(s, l, j, pivot_function)
+        quick_sort(s, i, r, pivot_function)
+        
+        
+
 def mid_pivot(l, r):
     return (l + r) // 2
 
@@ -30,7 +73,7 @@ def random_pivot(l, r):
     where l and r are of type 'int' """
     
     import random
-    return
+    return random.randint(l, r)
 
 def quick_sort_mid(s):
     quick_sort(s, 0, len(s) - 1, mid_pivot)
@@ -40,7 +83,7 @@ def quick_sort_rand(s):
     quick_sort(s, 0, len(s) - 1, random_pivot)
     return s
 
-quick_sort_faster = False
+quick_sort_faster = True
 merge_sort_faster = False
 
 matrikelnummern = [108018266786] # Enter your Matrikelnummer here
--- Info2/test_aufgabe2.py
+++ Info2/test_aufgabe2.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     s_s = ["".join([random.choice(chars) for i in range(6)]) for i in range(n)]
     s_n_b = [(random.randint(1, n-1), s_n[i]) for i in range(len(s_n))]
     s_s_b = [(random.randint(1, n-1), s_s[i]) for i in range(len(s_s))]
-    
+
     print("merge_sort works with numbers:", is_sorted(merge_sort(s_n.copy())))
     print("merge_sort works with strings:", is_sorted(merge_sort(s_s.copy())))
     print("quick_sort_mid works with numbers:", is_sorted(quick_sort_mid(s_n.copy())))
@@ -41,3 +41,24 @@ if __name__ == "__main__":
     print("quick_sort_mid time on string: {:5.3f}".format(measure_time(quick_sort_mid, s_s.copy())))
     print("quick_sort_rand time on numbers: {:5.3f}".format(measure_time(quick_sort_rand, s_n_b.copy())))
     print("quick_sort_rand time on string: {:5.3f}".format(measure_time(quick_sort_rand, s_s_b.copy())))
+
+
+    timekeeperQ = []
+    timekeeperM = []
+
+    for i in range(0, 20):
+        n = random.randint(1000, 100000)
+        k = random.randint(1000, 100000)
+
+        qarr1 = [random.randint(1000,k) for i in range(0, n)]
+        marr1 = qarr1.copy()
+
+        print("this is the ", i, "iteration")
+        timekeeperQ.append(measure_time(quick_sort_rand, qarr1))
+        timekeeperM.append(measure_time(merge_sort, marr1))
+
+        print("mergesort took", timekeeperM[i], "secs")
+        print("quicksort took", timekeeperQ[i], "secs")
+    
+    print("mergesort took on avg", sum(timekeeperM), "secs")
+    print("quicksort took on avg", sum(timekeeperQ), "secs")
