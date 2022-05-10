c2b697cc794cccb48702d39ec35ee99fae5b12c3
Namenot <aaron.sastry2@gmail.com>
2022-05-10 16:01:27 +0200

    started to make a example solution for the others

--- Betriebssysteme/wsortfueralle.c
+++ Betriebssysteme/wsortfueralle.c
@@ -2,8 +2,52 @@
 #include <stdlib.h>
 #include <errno.h>
 
-int main()
+struct subarray
 {
+    int size;
+    int *word;
+};
+
+typedef struct subarray subarray;
+
+void printArray(int *)
+{
+
+}
+
+int main(void)
+{
+
+    int ***array = malloc(0);
+    int arraysize = 0;
+
+    subarray sa;
+    sa.word = malloc(0);
+    sa.size = 0;
+
+    // load the entire content into an array of arrays
+    int c;
+    do
+    {  
+        c = fgetc(stdin);
+        if(c == '\n' || c == EOF)
+        {
+            if(sa.size > 0)
+            {
+                arraysize++;
+                array = realloc(array, sizeof(subarray *)* arraysize);
+                array[arraysize -1] = &sa.word;
+            }
+
+            free(sa.word);
+            sa.size = 0;
+            continue;
+        }
+        
+        sa.size ++;
+        sa.word = realloc(sa.word, sizeof(int) * sa.size);
+        sa.word[sa.size-1] = c;
+    }while(c != EOF);
 
     return 0;
 }
